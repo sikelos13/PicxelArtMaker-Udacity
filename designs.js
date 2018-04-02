424bce83f2e2482f562e04af246880c9c64af782
Stefanos Athansoulias <32720107+sikelos13@users.noreply.github.com>
2018-04-02 13:56:40 +0300

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,51 +1,72 @@
-
-function makeGrid(height,width) {
-  var height = $('#input_height').val();
-  var width = $('#input_width').val();
-  $("#pixel_canvas").html('');
-  for(var i=0;i<height;i++){
-    $("#pixel_canvas").append("<tr></tr>");
+function makeGrid() {
+  /* Set variables of height, width and
+  pixel canvas.Also clear the canvas*/
+  const height = $('#input_height').val();
+  const width = $('#input_width').val();
+  const table = $("#pixel_canvas");
+  table.html('');
+  /* Make the grid of the canvas.*/
+  for (let i = 0; i < height; i++) {
+    table.append("<tr></tr>");
   }
+  //Make cells of Canvas.
   $('tr').each(function() {
-    for(var j =0;j<width;j++){
+    for (let j = 0; j < width; j++) {
       $(this).append('<td></td>');
     }
   });
 }
-$(document).ready(function(){
-  var canvas= $("#pixel_canvas");
-  var color= $('#colorPicker');
-  $("#sizePicker").on("submit",function(e){
-    makeGrid();
+$(document).ready(function() {
+  const canvas = $("#pixel_canvas");
+  const color = $('#colorPicker');
+  /*From sizepicker submit the size of canvas and run makeGrid*/
+  $("#sizePicker").on("submit", function(e) {
     e.preventDefault();
-    });
-//when document is ready check if mouse is up or isDown
-  var isDown=false;
-  $(document).mousedown(function(e){
-    if(e.which===1){
+    makeGrid();
+  });
+  //when document is ready check if mouse is up or isDown
+  let isDown = false;
+  let isDownright = false;
+  //When mouse is down on left click the set variabvle to true else false
+  $(document).mousedown(function(e) {
+    if (e.which === 1) {
       isDown = true;
     }
-    });
-  $(document).mouseup(function(){  isDown= false; });
-  //set color from colorPicker
-  canvas.on('mousedown','td',function() {
-    if(isDown){
-      var colorCell = color.val();
-      $(this).attr('bgcolor',colorCell);
+  });
+  $(document).mouseup(function() {
+    isDown = false;
+  });
+  //If mouse is down then mouseover the canvas with coloro from colorPicker
+  canvas.on('mouseover', 'td', function(e) {
+    if (isDown) {
+      const colorCell = color.val();
+      $(this).css('background-color', colorCell);
     }
   });
-  //If mouse is down the mouseover the canvas
-  canvas.on('mouseover','td',function(e){
-    if(isDown){
-      var colorCell = color.val();
-      $(this).attr('bgcolor',colorCell);
+  /*Hold right click to erase
+  Disable right click menu for better handle or missclicks on canva*/
+  $(document).mousedown(function(e) {
+    if (e.which === 3) {
+      e.preventDefault();
+      isDownright = true;
     }
   });
-});
-//On right click set attribute to null
-//Disable right click menu for better handle or missclicks on canva
-$("#pixel_canvas").on('contextmenu','td',function(e){
-  e.preventDefault();
-  e.stopPropagation();
-  $(this).attr('bgcolor','');
+  $(document).mouseup(function() {
+    isDownright = false;
+  });
+  canvas.on('mouseover', 'td', function(e) {
+    if (isDownright) {
+      e.preventDefault();
+      $(this).css('background-color', '');
+    }
+  });
+  //Disavle content menu of right click
+  canvas.on('contextmenu', 'td', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+  //Toogle grid on or off
+  $('.toggleCell').click(function() {
+    $('td').toggleClass('hideGrid');
+  });
 });
